e5a036e94c67f4298e199d1dea40bca1a2f61e92
Fpsska <fpsska1337@gmail.com>
2022-06-22 13:54:53 +0300

    feat: create Header component, use in Layout.tsx & create image folder, add logo.svg

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { NavLink } from 'react-router-dom';
+
+import logo from '../../assets/images/logo.svg';
+
+import './header.scss';
+
+// /. imports
+
+const Header: React.FC = () => {
+    return (
+        <header className="header">
+            <div className="header__wrapper">
+
+                <a className="header__logo" href="#">
+                    <img className="header__image" src={logo} alt="logo" />
+                    <h2 className="header__title">Agency</h2>
+                </a>
+
+                <nav className="nav">
+                    <ul className="nav__menu">
+                        <li className="nav__item">
+                            <NavLink className="nav__link" to="/">About</NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink className="nav__link" to="/">Services</NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink className="nav__link" to="/">Pricing</NavLink>
+                        </li>
+                        <li className="nav__item">
+                            <NavLink className="nav__link" to="/">Blog</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+
+                <div className="header__contacts">
+                    <button className="header__button">CONTACT</button>
+                </div>
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
--- src/components/common/Layout/Layout.tsx
+++ src/components/common/Layout/Layout.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Outlet } from 'react-router';
 
+import Header from '../../Header/Header';
+
 // /. imports
 
 const Layout: React.FC = () => {
     return (
         <div className="page">
-            <header className="header">HEADER</header>
+            <Header/>
             <main className="main">
                 <Outlet/>
             </main>
