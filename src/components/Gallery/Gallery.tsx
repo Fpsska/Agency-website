37cc8df932c09a44d27bedd3abadb22822e60f06
Fpsska <fpsska1337@gmail.com>
2022-06-23 16:30:43 +0300

    feat: add logic of render gallery__message HTML-section, when galleryCards is empty

--- src/components/Gallery/Gallery.tsx
+++ src/components/Gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useAppSelector } from '../../app/hooks';
 
@@ -12,20 +12,36 @@ const Gallery: React.FC = () => {
 
     const { galleryCards } = useAppSelector(state => state.gallerySlice);
 
+    const [emptyStatus, setEmptyStatus] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (galleryCards.length === 0) {
+            setEmptyStatus(true);
+        } else {
+            setEmptyStatus(false);
+        }
+    }, [galleryCards]);
+
     return (
-        <div className="gallery__photos">
-            {galleryCards.map(item => {
-                return (
-                    <GalleryTemplate
-                        key={item.id}
-                        id={item.id}
-                        category={item.category}
-                        text={item.text}
-                        image={item.image}
-                        isActive={item.isActive}
-                    />
-                );
-            })}
+        <div className="gallery__body">
+            {emptyStatus
+                ?
+                <h3 className="gallery__message">no matches</h3>
+                :
+                <div className="gallery__photos">
+                    {galleryCards.map(item => {
+                        return (
+                            <GalleryTemplate
+                                key={item.id}
+                                id={item.id}
+                                category={item.category}
+                                text={item.text}
+                                image={item.image}
+                                isActive={item.isActive}
+                            />
+                        );
+                    })}</div>
+            }
         </div>
     );
 };
