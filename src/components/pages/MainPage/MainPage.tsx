277f4026826de4c088a6f8874649ca8e1ed87c35
Fpsska <fpsska1337@gmail.com>
2022-06-23 13:59:18 +0300

    feat: create handleSelect function in MainPage.tsx, add logic of filtering by gallery__select HTML-elements

--- src/components/pages/MainPage/MainPage.tsx
+++ src/components/pages/MainPage/MainPage.tsx
@@ -20,6 +20,26 @@ const MainPage: React.FC = () => {
         dispatch(fetchImagesData());
     };
 
+    const handleSelect = (value: string): void => {
+        switch (value) {
+            case 'all':
+                dispatch(filterGalleryByCategory({ category: value }));
+                break;
+            case 'design':
+                dispatch(filterGalleryByCategory({ category: value }));
+                break;
+            case 'branding':
+                dispatch(filterGalleryByCategory({ category: value }));
+                break;
+            case 'illustration':
+                dispatch(filterGalleryByCategory({ category: value }));
+                break;
+            case 'motion':
+                dispatch(filterGalleryByCategory({ category: value }));
+                break;
+        }
+    };
+
     return (
         <div className="main-page">
             <div className="main-page__wrapper">
@@ -48,12 +68,15 @@ const MainPage: React.FC = () => {
                             </ul>
                         </nav>
 
-                        <select className="gallery__select select" name="" id="" defaultValue="Show All">
-                            <option className="select__item" value="">Show All</option>
-                            <option className="select__item" value="">Design</option>
-                            <option className="select__item" value="">Branding</option>
-                            <option className="select__item" value="">illustration</option>
-                            <option className="select__item" value="">Motion</option>
+                        <select className="gallery__select select"
+                            defaultValue="Show All"
+                            onChange={(e) => handleSelect(e.target.value)}
+                        >
+                            <option className="select__item" value="all">Show All</option>
+                            <option className="select__item" value="design">Design</option>
+                            <option className="select__item" value="branding">Branding</option>
+                            <option className="select__item" value="illustration">illustration</option>
+                            <option className="select__item" value="motion">Motion</option>
                         </select>
 
                         <Gallery />
