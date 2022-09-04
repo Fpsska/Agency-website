25853621d04168b89edf1ac1a1601a099fb48362
Fpsska <fpsska1337@gmail.com>
2022-09-04 16:18:48 +0300

    refactor: HTML-str, styles of Preloader.tsx component & now use lazy func, Suspense component for lazy load all component of Layout Route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import { Routes, Route } from 'react-router';
 
 import Layout from '../common/Layout/Layout';
-import MainPage from '../pages/MainPage/MainPage';
-import ServicesPage from '../pages/ServicesPage/ServicesPage';
-import PricingPage from '../pages/PricingPage/PricingPage';
-import BlogPage from '../pages/BlogPage/BlogPage';
 
 import '../../assets/styles/_style.scss';
 import '../../assets/styles/_media.scss';
@@ -14,15 +10,35 @@ import './App.css';
 
 // /. imports
 
+const MainPageLazy = lazy(() => import('../pages/MainPage/MainPage'));
+const ServicesPageLazy = lazy(() => import('../pages/ServicesPage/ServicesPage'));
+const PricingPageLazy = lazy(() => import('../pages/PricingPage/PricingPage'));
+const BlogPageLazy = lazy(() => import('../pages/BlogPage/BlogPage'));
+
 const App: React.FC = () => {
   return (
     <div className="App">
       <Routes>
         <Route path="Jupiter-Task" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="Services" element={<ServicesPage />} />
-          <Route path="Pricing" element={<PricingPage />} />
-          <Route path="Blog" element={<BlogPage />} />
+          <Route index element={
+            <Suspense>
+              <MainPageLazy />
+            </Suspense>} />
+          <Route path="Services" element={
+            <Suspense>
+              <ServicesPageLazy />
+            </Suspense>
+          } />
+          <Route path="Pricing" element={
+            <Suspense>
+              <PricingPageLazy />
+            </Suspense>
+          } />
+          <Route path="Blog" element={
+            <Suspense>
+              <BlogPageLazy />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </div>
--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -5,22 +5,8 @@ import './preloader.scss';
 // /. imports
 
 const Preloader: React.FC = () => {
-
     return (
-        <div className="preloader">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+        <div className="loader"></div>
     );
 };
 
